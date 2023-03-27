475a9c6129555645868422e689cd273adb5d1f02
Retii <109904771+RetiiAyo@users.noreply.github.com>
2023-03-27 19:10:22 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,19 @@
-print("loading.. (0/3)");
+_G.scriptPacks = {}; _G.scriptPacks.settings = {
+	skipGameLoading = false, -- self explaining. (default: false)
+	delayBetweenExecutingScripts = 1, -- self explaining. (default: 1)
+	genreToLoad = "pvp" -- genres: pvp (more coming soon)
+};
+
+print("loading.. (0/4)");
 if not _G.scriptPacks then return print("_G.scriptPacks is missing."); end;
 if not _G.scriptPacks.settings then return print("_G.scriptPacks.settings is missing.") end;
 if not _G.scriptPacks.settings.skipGameLoading then return print("_G.scriptPacks.settings.skipGameLoading is missing.") end;
 if not _G.scriptPacks.settings.delayBetweenExecutingScripts then return print("_G.scriptPacks.settings.delayBetweenExecutingScripts is missing.") end;
 if not _G.scriptPacks.settings.genreToLoad then return print("_G.scriptPacks.settings.genreToLoad is missing.") end;
-print("_G.scriptPacks initialised! (1/3)");
-print("_G.scriptPacks.settings initialised! (2/3)");
+print("_G.scriptPacks initialised! (1/4)");
+print("_G.scriptPacks.settings initialised! (2/4)");
+repeat task.wait() until game:IsLoaded() or _G.scriptPacks.settings.skipGameLoading == true;
+print("game loaded (3/4)");
 
 local genres = {
 	pvp = loadstring(game:HttpGet("https://raw.githubusercontent.com/RetiiAyo/script-packs/main/genres/pvp.lua"));
@@ -20,4 +28,4 @@ else
 		wait(_g.scriptPacks.settings.delayBetweenExecutingScripts);
 	end;
 end;
-print("everything was successfully executed! (3/3)");
+print("everything was successfully executed! (4/4)");
