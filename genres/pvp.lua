da98abe4deae3ca8d68f41d30af446ecea38b08a
Retii <109904771+RetiiAyo@users.noreply.github.com>
2023-03-27 19:03:30 +0200

    Update pvp.lua

--- genres/pvp.lua
+++ genres/pvp.lua
@@ -1,3 +1,4 @@
 return {
-  "https://raw.githubusercontent.com/RetiiAyo/script-packs/main/scripts/aimbot.lua"
+  "https://raw.githubusercontent.com/RetiiAyo/script-packs/main/scripts/aimbot.lua",
+  "https://raw.githubusercontent.com/RetiiAyo/script-packs/main/scripts/esp.lua"
 }
