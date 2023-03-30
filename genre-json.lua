46e177a3605b99459e5b13f72b74fcb06e42581f
Retii <109904771+RetiiAyo@users.noreply.github.com>
2023-03-30 17:23:50 +0200

    Update genre-json.lua

--- genre-json.lua
+++ genre-json.lua
@@ -1,3 +1,4 @@
 return {
-	pvp = loadstring(game:HttpGet("https://raw.githubusercontent.com/RetiiAyo/script-packs/main/genres/pvp.lua"))()
+	pvp = loadstring(game:HttpGet("https://raw.githubusercontent.com/RetiiAyo/script-packs/main/genres/pvp.lua"))(),
+	universal = loadstring(game:HttpGet("https://raw.githubusercontent.com/RetiiAyo/script-packs/main/genres/universal.lua"))()
 }
