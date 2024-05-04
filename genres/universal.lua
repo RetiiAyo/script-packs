bf09ff9b1be6654a8bab11e270ea48891491232f
Retii <109904771+RetiiAyo@users.noreply.github.com>
2024-05-04 15:16:57 +0200

    Update universal.lua

--- genres/universal.lua
+++ genres/universal.lua
@@ -1,3 +1,4 @@
 return {
-  "https://raw.githubusercontent.com/RetiiAyo/script-packs/main/scripts/esp.lua"
+  "https://raw.githubusercontent.com/RetiiAyo/script-packs/main/scripts/esp.lua",
+  "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"
 }
