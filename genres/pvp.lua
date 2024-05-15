9f9d852876785f9d610ff35d337d4391258dd144
Retii <109904771+RetiiAyo@users.noreply.github.com>
2024-05-15 22:07:04 +0200

    Update pvp.lua

--- genres/pvp.lua
+++ genres/pvp.lua
@@ -1,5 +1,6 @@
 return {
   "https://raw.githubusercontent.com/RetiiAyo/script-packs/main/scripts/aimbot.lua",
   "https://raw.githubusercontent.com/RetiiAyo/script-packs/main/scripts/esp-v2.lua",
-  "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"
+  "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source",
+  "https://raw.githubusercontent.com/RetiiAyo/script-packs/main/scripts/optimizer.lua"
 }
